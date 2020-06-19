1227cb8963dc2c364941e6de12baf86ca513dc8c
Supreet <me.supi@gmail.com>
2020-06-19 06:20:42 -0400

    Added env variables

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export default {
     // API_URL: `https://afternoon-citadel-97734.herokuapp.com`,
     //API_KEY: process.env.REACT_APP_API_KEY,
-    TRAIL_URL: `https://www.hikingproject.com/data/get-trails`,
-    GEOCODE_URL: `https://maps.googleapis.com/maps/api/geocode/json`,
-    TRAIL_API_KEY: `200584708-6c2ed6bce77a3687c608095444a035bf`,
-    GEOCODE_API_KEY: `AIzaSyA_dsM-989v-ukkzPRkvC4wVUkqMyp2pb8`,
+    TRAIL_URL: process.env.TRAIL_URL,
+    GEOCODE_URL: process.env.GEOCODE_URL,
+    TRAIL_API_KEY: process.env.TRAIL_API_KEY,
+    GEOCODE_API_KEY: process.env.GEOCODE_API_KEY,
     API_ENDPOINT: "http://localhost:8000/api",
     TOKEN_KEY: "trails-to-go-client-auth-token",
 };
