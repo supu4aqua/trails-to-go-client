0e14823671dc34ae1312c19c17bac12cd26cc292
Supreet <supreet.kaur@live.com>
2020-06-29 16:14:59 -0400

    Removed api key

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
-    TRAIL_URL: `https://www.hikingproject.com/data/get-trails`,
-    GEOCODE_URL: `https://maps.googleapis.com/maps/api/geocode/json`,
-    TRAIL_API_KEY: `200584708-6c2ed6bce77a3687c608095444a035bf`,
-    GEOCODE_API_KEY: `AIzaSyA_dsM-989v-ukkzPRkvC4wVUkqMyp2pb8`,
-    API_ENDPOINT: "http://localhost:8000/api",
-    TOKEN_KEY: "trails-to-go-client-auth-token",
+  TRAIL_URL: `https://www.hikingproject.com/data/get-trails`,
+  GEOCODE_URL: `https://maps.googleapis.com/maps/api/geocode/json`,
+  TRAIL_API_KEY: `200584708-6c2ed6bce77a3687c608095444a035bf`,
+  // GEOCODE_API_KEY: `AIzaSyA_dsM-989v-ukk`,
+  API_ENDPOINT: "http://localhost:8000/api",
+  TOKEN_KEY: "trails-to-go-client-auth-token",
 };
